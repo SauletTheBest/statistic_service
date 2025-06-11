dffc0b5193b97e558b148a91e12dfa9099b14842
ernar <aubakirernar111@gmail.com>
2025-06-11 14:06:25 +0500

    aaaaa

--- internal/model/User.go
+++ internal/model/User.go
@@ -1,5 +1,32 @@
-package models
+package model
 
-type User struct{
-	
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
+)
+
+type User struct {
+	ID           uuid.UUID `db:"id"            json:"id"`
+	Email        string    `db:"email"         json:"email"`
+	PasswordHash string    `db:"password_hash" json:"-"`
+	CreatedAt    time.Time `db:"created_at"    json:"created_at"`
+}
+
+type Category struct {
+	ID     uuid.UUID `db:"id"      json:"id"`
+	UserID uuid.UUID `db:"user_id" json:"user_id"`
+	Name   string    `db:"name"    json:"name"`
+	Type   string    `db:"type"    json:"type"` // "expense" немесе "income"
+}
+
+type Transaction struct {
+	ID         uuid.UUID       `db:"id"          json:"id"`
+	UserID     uuid.UUID       `db:"user_id"     json:"user_id"`
+	Amount     decimal.Decimal `db:"amount"      json:"amount"`
+	Type       string          `db:"type"        json:"type"`
+	CategoryID *uuid.UUID      `db:"category_id" json:"category_id"` // null болуы мумкин
+	Comment    *string         `db:"comment"     json:"comment"`     // null болуы мумкин
+	CreatedAt  time.Time       `db:"created_at"  json:"created_at"`
 }
