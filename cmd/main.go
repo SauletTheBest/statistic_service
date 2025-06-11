30ddb5453bd3970d9ce96c11c371b1b413633a9a
SauletTheBest <231003@astanait.edu.kz>
2025-06-11 14:52:23 +0500

    commit

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"statistic_service/internal/config"
+	"statistic_service/internal/db"
+	"statistic_service/internal/handler"
+	"statistic_service/internal/repository"
+	"statistic_service/internal/service"
+	"statistic_service/internal/middleware"
+)
+
+func main() {
+
+	cfg := config.LoadConfig()
+	database := db.Connect(cfg.DBURL)
+
+	userRepo := repository.NewUserRepository(database)
+	authService := service.NewAuthService(userRepo, cfg.JWTSecret)
+	authHandler := handler.NewAuthHandler(authService)
+    authMiddleware := middleware.JWTAuth(cfg.JWTSecret)
+
+	r := gin.Default()
+	r.POST("/register", authHandler.Register)
+	r.POST("/login", authHandler.Login)
+
+	r.GET("/me", authMiddleware, authHandler.GetProfile)
+	r.Run(":" + cfg.Port)
+}
---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,17 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(userID, secret string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"exp":     time.Now().Add(24 * time.Hour).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
